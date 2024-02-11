102b65a71d820c54683ba0352e91050e29e7452f
Ludmi <ludmit@outlook.com>
2024-02-11 20:47:56 -0300

    fix: :bug: finish tests, fixed bugs

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ Route::group(['prefix' => '/tasks', 'as' => 'tasks.'], function () {
     Route::get('/{id}', [TaskController::class, 'get'])
     ->where('id', '[1-9][0-9]*');
     Route::post('/', [TaskController::class, 'store']);
-    Route::put('/', [TaskController::class, 'update'])
+    Route::put('/{id}', [TaskController::class, 'update'])
     ->where('id', '[1-9][0-9]*');
-    Route::delete('/', [TaskController::class, 'delete'])
+    Route::delete('/{id}', [TaskController::class, 'delete'])
     ->where('id', '[1-9][0-9]*');
     Route::put('/', [TaskController::class, 'reorder']);
 });
